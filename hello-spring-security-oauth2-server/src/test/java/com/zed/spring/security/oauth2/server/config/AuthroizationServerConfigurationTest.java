1265bf710a41cdcb0917f7e8c1f1683d179a41f7
Zed <shadowl91@163.com>
2020-02-22 23:28:43 +0800

    oauth2 server use jdbc to manage instead memory

---
+++ hello-spring-security-oauth2-server/src/test/java/com/zed/spring/security/oauth2/server/config/AuthroizationServerConfigurationTest.java
@@ -0,0 +1,25 @@
+package com.zed.spring.security.oauth2.server.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Zed
+ * @date 2020/2/22 下午11:00
+ * @contact shadowl91@163.com
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthroizationServerConfigurationTest {
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
+    @Test
+    public void test() {
+        System.out.println(passwordEncoder.encode("secret"));
+    }
+}
